d47e45e1f66d5bf775b5fc9ca95397e6b446c64c
Hussyvel Ribeiro <hussyribeiro@hotmail.com>
2022-04-02 00:15:51 -0300

    Todo list atualizado

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -51,9 +51,8 @@ class TodoListsController < ApplicationController
   # DELETE /todo_lists/1 or /todo_lists/1.json
   def destroy
     @todo_list.destroy
-
     respond_to do |format|
-      format.html { redirect_to todo_lists_url, notice: I18n.t('.destroy') }
+      format.html { redirect_to todo_lists_url, notice: I18n.t('.destroy') } #notice é um parâmetro e o resto é o valor, um utilitário para tradução
       format.json { head :no_content }
     end
   end
